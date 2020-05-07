4c5e9f511a5af24fb795528e730c2bac85c30a61
Marina Melaré <melare.nina@gmail.com>
2020-05-07 14:35:56 -0300

    Using Animation Callbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -163,4 +163,12 @@ export class AppComponent {
     onShrink(){
       this.wildState = 'shrunken';
     }
+
+    animationStarted(event){
+      console.log(event);
+    }
+
+    animationEnded(event){
+      console.log(event);
+    }
 }
