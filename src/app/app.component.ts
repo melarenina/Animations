b741e0161f863a7ecbca68a2f9fbb166926f5c8c
Marina Melaré <melare.nina@gmail.com>
2020-05-07 14:29:51 -0300

    Using Keyframes for Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -103,15 +103,23 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
         animate(1000, keyframes([
           style({
             transform: 'translateX(-100px)',
-            opacity: 0
+            opacity: 0,
+            offset: 0
           }),
           style({
             transform: 'translateX(-50px)',
-            opacity: 0.5
+            opacity: 0.5,
+            offset: 0.3
           }),
           style({
             transform: 'translateX(-20px)',
-            opacity: 1
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
           })
         ]))
       ]),
